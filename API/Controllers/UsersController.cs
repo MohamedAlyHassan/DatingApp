4fa680f9cb262339b56f99cfb2713f14ae1cc3cd
MohamedAlyHassan <76614167+MohamedAlyHassan@users.noreply.github.com>
2021-08-24 15:16:21 +0200

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -25,7 +26,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous] 
+        
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
             var users = await _userRep.GetMembersAsync();
@@ -33,12 +34,28 @@ namespace API.Controllers
             
         }
 
-         [Authorize]
+        
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRep.GetMemberAsync(username);
              
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult> updateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRep.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto,user);
+
+            _userRep.Update(user);
+
+            if (await _userRep.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
+import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanDeactivate } from '@angular/router';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
@@ -17,8 +19,9 @@ const routes: Routes = [
     runGuardsAndResolvers:'always',
     canActivate: [AuthGuard],
     children: [
-  {path:'members', component:MemberListComponent, canActivate:[AuthGuard]},
+  {path:'members', component:MemberListComponent,},
   {path:'members/:username', component:MemberDetailsComponent},
+  {path:'member/edit', component:MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
   {path:'lists', component:ListsComponent},
   {path:'messages', component:MessagesComponent},
     ]
